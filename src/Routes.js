cd711667dcae8ef79b1b4476afd780eee20d1189
Herison Pereira <herisonup@gmail.com>
2024-01-18 02:58:12 -0300

    Feature: add qrcode to large screen

--- src/Routes.js
+++ src/Routes.js
@@ -7,15 +7,18 @@ import Dashboard from './pages/Dashboard';
 import Amulets from './pages/Dashboard/Amulets';
 import Statistic from './pages/Dashboard/Statistic';
 import whiteLogo from './assets/logo/logo-white.svg';
+import qrcode from './assets/images/qrcode.png';
 import { BalanceProvider } from './context/balance';
 
 export default function Routes() {
   return (
     <>
       <div className="max-sm:hidden bg-blue_main flex flex-col w-full min-h-screen justify-center items-center text-white">
-        <img src={whiteLogo} alt="logo" className="mb-4 h-auto w-64" />
-        <h2>Acesse nosso site através de seu smartphone</h2>
-        <div className="bg-white rounded-lg p-8 mt-8">QR CODE</div>
+        <img src={whiteLogo} alt="logo" className="h-auto w-64" />
+        <div className="bg-white rounded-lg mt-8 w-60 overflow-hidden">
+          <img src={qrcode} alt="QRCODE" className="w-full h-auto" />
+        </div>
+        <h2 className="mt-8">Acesse nosso site através de seu smartphone</h2>
       </div>
       <div className="sm:hidden min-h-screen flex flex-col bg-white">
         <BalanceProvider>
